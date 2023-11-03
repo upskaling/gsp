b4d1276284d016c5551e63256c191ba500d9f61d
upskaling <upskaling@users.noreply.github.com>
2023-11-03 19:47:53 +0100

    Refonte de la méthode de saisie du presse-papiers.

--- src/input/clipboard.rs
+++ src/input/clipboard.rs
@@ -6,7 +6,7 @@ pub struct Clipboard {}
 impl InputEngine for Clipboard {
     fn input(&self) -> String {
         let mut ctx: ClipboardContext = ClipboardProvider::new().unwrap();
-        let contents = ctx.get_contents().unwrap();
-        contents
+        
+        ctx.get_contents().unwrap()
     }
 }
