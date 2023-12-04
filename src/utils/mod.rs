c53368acd9475ff63cddd61734b64551ed9bb431
upskaling <upskaling@users.noreply.github.com>
2023-12-04 10:15:05 +0100

    fix: :bug: fix get_pidof function

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,16 +1,24 @@
-use psutil::process::processes;
+use psutil::process::{processes, Process, ProcessError};
 
 pub mod textutils;
 
 pub fn get_pidof(process_name: &str) -> Vec<u32> {
-    let mut processes_list = Vec::new();
-    for process in processes().unwrap() {
-        let process = process.unwrap();
+    let processes = processes().unwrap().into_iter();
 
-        if process.name().unwrap() == process_name {
-            processes_list.push(process.pid());
-        }
+    processes
+        .filter_map(|p| get_pid(p, process_name))
+        .collect::<Vec<u32>>()
+}
+
+fn get_pid(process: Result<Process, ProcessError>, process_name: &str) -> Option<u32> {
+    let p = match process {
+        Ok(p) => p,
+        Err(_) => return None,
+    };
+
+    if p.name().unwrap_or_else(|_| "".to_string()) == process_name {
+        return Some(p.pid());
     }
 
-    processes_list
+    None
 }
