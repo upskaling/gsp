f002c098ad32138d1cfed1668b0dc583e80f00cd
upskaling <upskaling@users.noreply.github.com>
2024-04-01 12:44:37 +0200

    refactor: utiliser la sortie stdout de tesseract

--- src/input/ocr/tesseract.rs
+++ src/input/ocr/tesseract.rs
@@ -1,6 +1,6 @@
 use std::process::{Command, Stdio};
 
-// tesseract -l fra /tmp/screenshot.png /dev/shm/screenshot
+// tesseract /tmp/screenshot.png -l fra stdout
 pub fn tesseract(screenshooter: &str, lang: &str) -> String {
     let lang = match lang {
         "de-DE" => "deu",
@@ -11,25 +11,16 @@ pub fn tesseract(screenshooter: &str, lang: &str) -> String {
         _ => "eng",
     };
 
-    let screenshot = "/dev/shm/screenshot";
-
-    Command::new("tesseract")
+    let command = Command::new("tesseract")
+        .arg(screenshooter)
+        .arg("stdout")
         .arg("-l")
         .arg(lang)
-        .arg(screenshooter)
-        .arg(screenshot)
         .stderr(Stdio::null())
         .output()
         .expect("failed to execute process");
 
-    // lire le fichier /dev/shm/screenshot.txt
-    let output = std::fs::read_to_string(format!("{}.txt", screenshot))
-        .unwrap()
-        .trim()
-        .to_string()
-        .replace(['\n', '\r'], " ");
+    let stdout = String::from_utf8_lossy(&command.stdout);
 
-    std::fs::remove_file(format!("{}.txt", screenshot)).unwrap();
-
-    output
+    stdout.to_string()
 }
